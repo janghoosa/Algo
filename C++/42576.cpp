bc48e0cee7ea0feac0166fe347d8ba9b9814d024
rehoosa <iuhoosa@gmail.com>
2022-02-19 02:30:55 +0900

    add 42576.cpp

---
+++ C++/42576.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+
+    for (int i = 0; i < participant.size(); i++) {
+        if (participant[i] == completion[i]) {
+            continue;
+        } else {
+            answer = participant[i];
+            break;
+        }
+    }
+    if (answer == "") {
+        answer = *(participant.end() - 1);
+    }
+    return answer;
+}
