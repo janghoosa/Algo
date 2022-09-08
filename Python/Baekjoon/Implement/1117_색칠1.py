bbe7b2eadddb676163aeedbc0752203c7f943864
janghoosa <janghoosa@gmail.com>
2022-09-08 16:14:57 +0900

    add: Baekjoon Implementation 1117 색칠 1

---
+++ Python/Baekjoon/Implement/1117_색칠1.py
@@ -0,0 +1,13 @@
+# 1117 색칠
+# https://www.acmicpc.net/problem/1117
+W,H,f,c,x1,y1,x2,y2 = map(int, input().split())
+
+doubledx = min(f, W-f)
+
+if x1 <= doubledx and doubledx <= x2: # 색칠이랑 겹친 부분이 겹칠 때
+    area = (x2-x1) * (y2-y1) * (c+1) + (doubledx-x1) * (y2-y1) * (c+1)
+elif x1 <= doubledx and x2 <= doubledx:  # 색칠이랑 겹친 부분이 아예 겹칠 때
+    area = 2 * (x2-x1) * (y2-y1) * (c+1)
+else:
+    area = (x2-x1) * (y2-y1) * (c+1)
+print(W*H-area)
