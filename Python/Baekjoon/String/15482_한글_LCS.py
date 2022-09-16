b9a4be5e51f6941b30ec8ec613550e60eacffec0
janghoosa <janghoosa@gmail.com>
2022-09-16 14:05:45 +0900

    add: Baekjoon String 15482 한글 LCS

---
+++ Python/Baekjoon/String/15482_한글_LCS.py
@@ -0,0 +1,12 @@
+# 15482 한글 LCS
+# https://www.acmicpc.net/problem/15482
+s1, s2 = input(), input()
+dp = [0] * 1000
+for i in range(len(s1)):
+    max_dp = 0
+    for j in range(len(s2)):
+        if max_dp < dp[j]:
+            max_dp = dp[j]
+        elif s1[i] == s2[j]:
+            dp[j] = max_dp + 1
+print(max(dp))
