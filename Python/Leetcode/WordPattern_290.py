335450de5f9f1612e98be420ce6ec7f6b34dac48
janghoosa <janghoosa@gmail.com>
2025-10-10 17:16:54 +0900

    add: LeetCode - Word Pattern

---
+++ Python/Leetcode/WordPattern_290.py
@@ -0,0 +1,29 @@
+# Word Pattern
+# https://leetcode.com/problems/word-pattern/
+# 패턴과 문자열이 일치하는지 확인하는 문제
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+
+        char_to_word = {}
+        word_to_char = {}
+
+        for p, w in zip(pattern, words):
+            if p in char_to_word:
+                if char_to_word[p] != w:
+                    return False
+            else:
+                char_to_word[p] = w
+
+            if w in word_to_char:
+                if word_to_char[w] != p:
+                    return False
+            else:
+                word_to_char[w] = p
+
+        return True      
